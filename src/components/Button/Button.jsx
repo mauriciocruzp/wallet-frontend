c0738dbcab06c03756e945ccae25e7fb431dc5d4
Mauricio Cruz <mawcp7@gmail.com>
2023-11-24 10:59:40 -0600

    Add Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,19 @@
+import "../../index.css";
+import Colors from "../../consts/Colors.js";
+
+const Button = ({ type, icon, primary, label, size, handleClick }) => {
+  const mode = primary ? Colors.background.primary : Colors.background.secondary;
+  const classes = [
+    mode,
+    size,
+    "rounded text-center flex justify-center items-center text-white py-2 px-7",
+  ].join(" ");
+  return (
+    <button className={classes} type={type} onClick={handleClick}>
+      {icon && <img src={icon} alt={label} className="mr-2 h-5" />}
+      {label}
+    </button>
+  );
+};
+
+export default Button;
